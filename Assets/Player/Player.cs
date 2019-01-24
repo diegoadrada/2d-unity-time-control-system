ae4e40dd9c7bf40f10ca6745104ff5a5526e4e44
diegoadrada <diegoadrada@gmail.com>
2019-01-24 17:09:04 -0500

    Basic 2D Player Movement with Rigidbody. (Vertical and horizontal)

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿/*
+* Copyright (c) Diego Adrada
+* http://www.diegoadrada.com/
+*/
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public struct InputNames
+    {
+        public const string Horizontal = "Horizontal";
+        public const string Vertical = "Vertical";
+    }
+
+    public float playerSpeed = 10f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Rigidbody2D myRigidbody2D;
+
+    private void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw(InputNames.Horizontal);
+        verticalInput = Input.GetAxisRaw(InputNames.Vertical);
+    
+        myRigidbody2D.velocity = new Vector2(horizontalInput * playerSpeed, verticalInput * playerSpeed);
+    }
+}
